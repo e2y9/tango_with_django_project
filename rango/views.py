f8a35822c6140884e0cc96e13836ad2ce8562a96
Emmet Young <emmetkyoung@gmail.com>
2020-01-23 21:00:51 +0000

    chapter 6 complete

--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,7 @@ def index(request):
     # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
 
     # NOTES for ^^^
     # Here, the expression Category.objects.order_by('-likes')[:5] queries the Category
@@ -24,6 +25,7 @@ def index(request):
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = pages_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
